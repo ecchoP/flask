90fe5eb3f889e18b2bfdadcbb74657347b0b4fc9
Aymeric Perraud <aymeric.perraud@gmail.com>
2020-04-25 18:50:52 -0400

    [hello-world.py] fixed url

--- hello-world.py
+++ hello-world.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     API_KEY = 'RGAPI-18a55044-ea70-4396-8814-ec802f4a0f80'
-    URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Callum%20Koi?api_key=%s' % (API_KEY)
+    URL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/CallumKoi?api_key=%s' % (API_KEY)
     riotapilevel = 'summonerLevel'
     r = requests.get(URL)
     level = r.json()[riotapilevel]
     return 'Callum\'s Summoner Level is currently:%s<br>Python Version: %s' % (level, '.'.join(['%d'%k for k in sys.version_info[:3]]))
-
+    #return 'hello'
 
 
 
